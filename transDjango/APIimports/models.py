0f12ca7d7c7fad87b4014267c7350767254891e6
saashimi <saavedra.kevin@gmail.com>
2017-02-02 10:09:19 -0800

    Fix syntax error in models.Model.

--- transDjango/APIimports/models.py
+++ transDjango/APIimports/models.py
@@ -4,16 +4,15 @@ from django.db import models
 from .modelDefinitions.CIPpoint import *
 
 
-from django.db import models
 from django.contrib.postgres import fields as pg_fields
 
-class ApiElement(Models.model):
+class ApiElement(models.Model):
     # The actual object represented by the api
     payload = pg_fields.JSONField()
     queryTime = models.DateTimeField()
     url = models.CharField(max_length=2083)
 
-class Project(Models.model):
+class Project(models.Model):
     geom = models.MultiPointField()
     bureau = models.CharField(max_length=1000)
     contact = models.CharField(max_length=1000)
