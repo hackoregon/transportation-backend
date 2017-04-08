a248c3afa3249b4e967eb33b5c97e7a733163a8c
Tony Fuentes <leobender@gmail.com>
2017-04-08 16:58:04 -0700

    remote db is now connected

--- transDjango/start-api-server.sh
+++ transDjango/start-api-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # cp /code/transDjango/settings_local_example.py /code/transDjango/settings_local.py
 
-sleep 100000
-./manage.py migrate
-./manage.py createcachetable
+# sleep 100000
+# ./manage.py migrate
+# ./manage.py createcachetable
 # ./manage.py import_jsons
 #  ./manage.py ingest_jsons
 #./manage.py runserver
