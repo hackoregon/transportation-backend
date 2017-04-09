c6e809d37d871e04d0125db75bebd4497c4733d8
saashimi <saavedra.kevin@gmail.com>
2017-04-09 14:56:19 -0700

    Fix sourceName in constants_local.py for clarity.

--- transDjango/APIimports/constants_local.py
+++ transDjango/APIimports/constants_local.py
@@ -6,13 +6,13 @@ LOCAL_API_META = {
 
     'Grind and Pave': {
         'uri': '../management/commands/datafiles/grind_pave.csv',
-        'sourceName': 'G_P Schedule as of 1-5-2017.pdf',
+        'sourceName': 'Grind and Pave',
         'startDateField': 'start',
         'endDateField': 'finish',
     },
     'Pavement Moratorium': {
         'uri': '../management/commands/datafiles/pavement_moratorium.csv',
-        'sourceName': 'Pavement Moratorium.csv',
+        'sourceName': 'Pavement Moratorium',
         'startDateField': 'start',
         'endDateField': 'finish',
     },
