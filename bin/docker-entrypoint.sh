43e83630456ce529d6671ac2ccd7dfdc241084f1
Tony Fuentes <leobender@gmail.com>
2017-04-08 17:50:49 -0700

    one more time

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
 ./bin/getconfig.sh
-echo "HERE"
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn transDjango.wsgi:application -b :8000
