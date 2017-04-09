89bb69677c699748da1b8794f6264fa80088c5a1
Tony Fuentes <leobender@gmail.com>
2017-04-08 17:22:35 -0700

    update docker api file

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
 ./bin/getconfig.sh
+pwd
+ls -la
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn homelessAPI.wsgi:application -b :8000
