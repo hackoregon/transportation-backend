0339ec557abbb8c47e47c790502bd1c38448f68e
mikethecanuck <mikethecanuck@gmail.com>
2017-04-09 19:12:34 -0700

    this gets a working container that builds locally so let's see if it builds and deploys successfully

--- transDjango/transDjango/settings.py
+++ transDjango/transDjango/settings.py
@@ -147,7 +147,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/transport/static/'
+
+# This seems to be necessary to enable the Django app to correctly style
+# the Swagger wrapper when the Django app runs inside a Docker container
+STATIC_ROOT = 'staticfiles'
+
 
 DATABASES = {
    'default': {
