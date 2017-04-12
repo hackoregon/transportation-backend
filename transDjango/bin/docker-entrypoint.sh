7f6f987685d2a7e17e76ab9b72aa9cab435fd803
Tony Fuentes <leobender@gmail.com>
2017-04-11 20:57:52 -0700

    enable gevent

--- transDjango/bin/docker-entrypoint.sh
+++ transDjango/bin/docker-entrypoint.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:~/.local/bin
 # python manage.py migrate --noinput
 # python manage.py collectstatic --noinput
 # gunicorn transDjango.wsgi:application -b :8000
-gunicorn budget_proj.wsgi:application -b :8000 --keep-alive 60 --worker-class 'gevent' # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
+gunicorn transDjango.wsgi:application -b :8000 --keep-alive 60 --worker-class 'gevent' # gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
