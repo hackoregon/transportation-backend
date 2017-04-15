e7d2af6b424686bed51756f77dc6ee148e4e8670
Tony Fuentes <leobender@gmail.com>
2017-04-15 15:23:44 -0700

    update urls.py

--- transDjango/APIs/urls.py
+++ transDjango/APIs/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^features/$', views.FeatureView.as_view(), name='features'),
     url(r'^features/(?P<pk>[0-9]+)/$', views.FeatureDetailView.as_view()),
     url(r'^conflicts/$', views.ConflictView.as_view(), name='conflicts'),
