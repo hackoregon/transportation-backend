0d6e1afb7058b458eac28ee7e6fd037cc79cb622
Tony Fuentes <leobender@gmail.com>
2017-04-15 11:21:35 -0700

    merge with master and update requirements

--- transDjango/bin/start-api-server.sh
+++ transDjango/bin/start-api-server.sh
@@ -5,7 +5,7 @@
 # ./manage.py migrate
 # ./manage.py createcachetable
 # ./manage.py import_jsons
-#  ./manage.py ingest_jsons
+# ./manage.py ingest_jsons
 #./manage.py runserver
 ./manage.py runserver 0.0.0.0:8000
 # gunicorn transDjango.wsgi:application -b :8000
