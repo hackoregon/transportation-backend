23bfe0178bd5a154955efb7a916462f61cdf6d50
Tony Fuentes <leobender@gmail.com>
2017-04-15 11:48:46 -0700

    add docker entrypoint

---
+++ bin/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo  Running docker-entrypoint.sh...
+
+#./bin/getconfig.sh
+#python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+# Fire up a lightweight frontend to host the Django endpoints - gunicorn was the default choice
+# gevent used to address ELB/gunicorn issue here https://github.com/benoitc/gunicorn/issues/1194
+gunicorn budget_proj.wsgi:application -b :8000 --worker-class 'gevent' --workers 3
