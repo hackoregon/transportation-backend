3be533aeec179b3c619bae18e11e328a02342dab
Tony Fuentes <leobender@gmail.com>
2017-04-15 15:27:59 -0700

    update

--- transDjango/transDjango/settings.py
+++ transDjango/transDjango/settings.py
@@ -19,6 +19,17 @@ from . import project_config
 SECRET_KEY = project_config.DJANGO_SECRET_KEY
 ALLOWED_HOSTS = project_config.ALLOWED_HOSTS
 
+# Get the IPV4 address we're working with on AWS
+# The Loadbalancer uses this ip address for healthchecks
+EC2_PRIVATE_IP = None
+try:
+    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
+except requests.exceptions.RequestException:
+    pass
+
+if EC2_PRIVATE_IP:
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
