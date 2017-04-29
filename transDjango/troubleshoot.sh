4847ffec605588ec246a1fcb57bf722214ff9d9b
Tony Fuentes <leobender@gmail.com>
2017-04-29 16:06:12 -0700

    try another endpoint

--- transDjango/troubleshoot.sh
+++ transDjango/troubleshoot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sleep 5
 echo "fetching conflicts endpoint"
-curl -X GET --header 'Accept: application/json' --header 'X-CSRFToken: S4EntS1yRZ0Puo6mLA4VZQ3fMWyZ7EdNwEnwlVLUtywaPy09Llz4lNOGpM21jUBH' 'http://localhost:8000/transport/conflicts/'
+# curl -X GET --header 'Accept: application/json' --header 'X-CSRFToken: S4EntS1yRZ0Puo6mLA4VZQ3fMWyZ7EdNwEnwlVLUtywaPy09Llz4lNOGpM21jUBH' 'http://localhost:8000/transport/conflicts/'
+curl -X GET --header 'Accept: text/html' --header 'X-CSRFToken: YEMk4i6IFzwPtJ8mCmHFbP0DkKop7mBTCevtWlQ4h82aOT29C7cOxML4XASrjCZN' 'http://localhost:8000/transport/nearby/'
+
 echo "got it"
