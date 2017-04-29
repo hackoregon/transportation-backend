d079ec6f7d3a11d22a4e1effe722ac7f08dbdc9e
Tony Fuentes <leobender@gmail.com>
2017-04-29 16:38:17 -0700

    tweak logging

--- transDjango/troubleshoot.sh
+++ transDjango/troubleshoot.sh
@@ -2,6 +2,6 @@
 sleep 5
 echo "fetching conflicts endpoint"
 # curl -X GET --header 'Accept: application/json' --header 'X-CSRFToken: S4EntS1yRZ0Puo6mLA4VZQ3fMWyZ7EdNwEnwlVLUtywaPy09Llz4lNOGpM21jUBH' 'http://localhost:8000/transport/conflicts/'
-curl -X GET --header 'Accept: text/html' --header 'X-CSRFToken: YEMk4i6IFzwPtJ8mCmHFbP0DkKop7mBTCevtWlQ4h82aOT29C7cOxML4XASrjCZN' 'http://localhost:8000/transport/nearby/'
+curl -X GET 'http://localhost:8000/transport/features/?source_name=Grind%20and%20Pave&startDate=2017-04-29&endDate=2020-01-01&format=json'
 
 echo "got it"
