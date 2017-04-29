1ee996326ddf988056b54e0adeef031b0d20f18a
Tony Fuentes <leobender@gmail.com>
2017-04-29 16:49:44 -0700

    try conflicts

--- transDjango/troubleshoot.sh
+++ transDjango/troubleshoot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sleep 5
 echo "fetching conflicts endpoint"
-# curl -X GET --header 'Accept: application/json' --header 'X-CSRFToken: S4EntS1yRZ0Puo6mLA4VZQ3fMWyZ7EdNwEnwlVLUtywaPy09Llz4lNOGpM21jUBH' 'http://localhost:8000/transport/conflicts/'
-curl -X GET 'http://localhost:8000/transport/features/?source_name=Grind%20and%20Pave&startDate=2017-04-29&endDate=2020-01-01&format=json'
-
+curl -X GET 'http://localhost:8000/transport/conflicts/'
+# curl -X GET 'http://localhost:8000/transport/features/?source_name=Grind%20and%20Pave&startDate=2017-04-29&endDate=2020-01-01&format=json'
 echo "got it"
