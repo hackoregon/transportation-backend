2ced6f07d0ed8627d0e515aa3fa024c95a944d32
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-30 04:46:58 -0700

    all the filter terms are now here

--- api-docs/filter.sql
+++ api-docs/filter.sql
@@ -3,4 +3,9 @@ WHERE canonical_status != 'COMPLETE'
   AND canonical_status != 'COMPLETED'
   AND canonical_status != 'REQUESTED'
   AND canonical_status != 'CANCELED'
-  AND canonical_status != 'DENIED';
+  AND canonical_status != 'DENIED'
+  AND NOT isempty(canonical_daterange)
+  AND neighborhood_id IS NOT NULL
+  AND lower(canonical_daterange) >= '2015-01-01'
+  AND upper(canonical_daterange) <= '2020-01-01'
+ORDER BY canonical_daterange DESC;
