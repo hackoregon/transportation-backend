902b8ac2f1f2fe410bd57a486250ed855b578c1a
Tony Fuentes <leobender@gmail.com>
2017-04-29 17:13:10 -0700

    try logging conflicts again

--- transDjango/troubleshoot.sh
+++ transDjango/troubleshoot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 30
+sleep 10
 echo "fetching conflicts endpoint"
 curl -X GET 'http://localhost:8000/transport/conflicts/'
 # curl -X GET 'http://localhost:8000/transport/features/?source_name=Grind%20and%20Pave&startDate=2017-04-29&endDate=2020-01-01&format=json'
