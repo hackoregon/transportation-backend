5d7c1b8f1072bf37e5cf70e91592810487e87a8c
nam20485 <nmiller217@gmail.com>
2017-10-30 12:17:23 -0700

    add shell script with steps from transDjango/README.md that fixes psycopg2 python build error from issue #49

---
+++ transDjango/setup-linux.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# USAGE:
+# $ chmod +x setup-linux.sh
+# $ ./setup-linux.sh
+#
+# OR:
+# $ bash setup-linux.sh
+
+# if this script is executing then we are already inside of a working copy tree cloned from the repo
+#git clone https://github.com/hackoregon/transportation-backend.git
+#cd transportation-backend
+
+# install apt packages missing from a clean Linux Mint 18.2 install
+sudo apt-get install -y virtualenv libpq-dev python3-dev postgresql-contrib
+
+# create and activate a virtualenv
+virtualenv -p python3 venv
+source venv/bin/activate
+
+# install python module requirements into virtualenv
+pip install -r requirements.txt
+
+# MacOS Homebrew packages?
+#brew install gdal
+#brew install libgeoip
+
+# install equivalent apt packages for Debian/Ubuntu/Mint systems
+sudo apt-get install -y gdal-bin libgeoip1
+
+# this command fails on my system
+#psql postgres
