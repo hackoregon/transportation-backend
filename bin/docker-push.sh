1a10531d24a4d4ca7c8ddacd5ec97fe6d1bd95b4
Mike Lonergan <mikethecanuck@gmail.com>
2018-05-07 17:37:25 -0700

    Removed redundant comments

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -2,14 +2,14 @@
 
 # Tag, Push and Deploy only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  # Push only if we're testing the master branch
+  # Tag, Push and Deploy only if we're on the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     export PATH=$PATH:$HOME/.local/bin
-    echo Getting the ECR login... # Troubleshooting
+    echo Getting the ECR login...
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
-    echo Running docker push command... # Troubleshooting
+    echo Running docker push command...
     docker push "$DOCKER_REPO"/"$DEPLOY_TARGET"/"$DOCKER_IMAGE":latest
-    echo Running ecs-deploy.sh script... # Troubleshooting
+    echo Running ecs-deploy.sh script...
     ./bin/ecs-deploy.sh  \
      --service-name "$ECS_SERVICE_NAME" \
      --cluster "$ECS_CLUSTER"   \
